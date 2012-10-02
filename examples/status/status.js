309f3254b46b2ed21be63f05f530b0ee92187bb8
Chad Engler <Chad.Engler@patlive.com>
2012-10-02 12:18:31 -0400

    add examples

---
+++ examples/status/status.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+var esl = require('modesl'),
+//open a connection
+conn = new esl.Connection('127.0.0.1', 8021, 'ClueCon', function() {
+    //send the status api command
+    conn.api('status', function(res) {
+	//log result body and exit
+        console.log(res.getBody());
+	process.exit(0);
+    });
+});

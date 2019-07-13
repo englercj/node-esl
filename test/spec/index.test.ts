10e43161e012d90a9418b72f64e1275441fd4b38
Chad Engler <englercj@live.com>
2019-07-13 08:36:42 -0700

    working on tests

---
+++ test/spec/index.test.ts
@@ -0,0 +1,16 @@
+import * as esl from '../../src';
+import { expect } from 'chai';
+
+describe('Event Socket Library', function()
+{
+    it('should have the correct exports', function()
+    {
+        expect(esl.setLogLevel).to.be.a('function');
+        expect(esl.eslSetLogLevel).to.be.a('function');
+
+        expect(esl.Event).to.be.a('function');
+        expect(esl.Connection).to.be.a('function');
+        expect(esl.Server).to.be.a('function');
+        expect(esl.Parser).to.be.a('function');
+    });
+});
